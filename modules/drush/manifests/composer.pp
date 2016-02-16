0d8025c6b7af04bd95046aaba5ab94427acb4636
Sammy Spets <sammys@sammyspets.com>
2016-02-16 12:11:46 +0700

    Ensure composer is installed using sudo.

--- modules/drush/manifests/composer.pp
+++ modules/drush/manifests/composer.pp
@@ -8,7 +8,7 @@ class drush::composer {
   exec { "installcomposer":
     path    => "/bin:/usr/bin",
     unless  => "ls /usr/local/bin | grep composer",
-    command => "curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer",
+    command => "curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer",
     require => [Class["phpfpm::extensions::curl"], Class["phpfpm::extensions::apc"], Class["php5::install"]],
   }
 
